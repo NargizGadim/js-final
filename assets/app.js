3a638a827574d15d596745edb7cd0227c9f009ba
Nargiz Gadim <nargizgadimova1@gmail.com>
2023-06-05 20:09:16 +0400

    projects section  is completed

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,4 @@
+
 let isOpen = true;
 function openCard() {
   document.querySelector(".card").classList.toggle("active");
@@ -9,3 +10,34 @@ function openCard() {
     };
   }
 }
+
+document.addEventListener("scroll", () => {
+  const header = document.querySelector("header");
+
+  // document.querySelector("header").classList.toggle("scrolled");
+
+  if (window.scrollY > 0) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
+
+// Swiper JS code
+
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
